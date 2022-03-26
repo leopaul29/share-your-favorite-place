381b4ed184de62eda9d61852c2f3cb246492a86f
Leo-paul <leopaulmartin@hotmail.fr>
2022-03-27 00:04:38 +0100

    add about component

---
+++ src/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h3>About</h3>
+      <p>
+        <a
+          href="https://www.flaticon.com/free-icons/location"
+          title="location icons"
+        >
+          Location icons created by Yogi Aprelliyanto - Flaticon
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default About;
